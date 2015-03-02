588ed38a0c2afe50f0e275f6d8abbb76bfd46298
Justin <jmguarino@sonic.net>
2015-03-01 18:55:46 -0800

    Testing overwrite of 15 puzzle with new project.

---
+++ Ghettris/Block.h
@@ -0,0 +1,13 @@
+//
+//  Block.h
+//  Ghettris
+//
+//  Created by Justin Guarino on 3/1/15.
+//  Copyright (c) 2015 JustinGuarino. All rights reserved.
+//
+
+#ifndef Ghettris_Block_h
+#define Ghettris_Block_h
+
+
+#endif
---
+++ Ghettris/Block.m
@@ -0,0 +1,9 @@
+//
+//  Block.m
+//  Ghettris
+//
+//  Created by Justin Guarino on 3/1/15.
+//  Copyright (c) 2015 JustinGuarino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
